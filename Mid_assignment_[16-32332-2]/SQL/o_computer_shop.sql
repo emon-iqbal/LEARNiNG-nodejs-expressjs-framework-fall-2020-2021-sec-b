887f35716ca97b8f00e2154e2d847015aaf70b27
emon-iqbal <rasjbii@gmail.com>
2020-12-04 16:13:25 +0600

    SQL file of THIS Computer Shop
    phpMyAdmin database collected

--- Mid_assignment_[16-32332-2]/SQL/o_computer_shop.sql
+++ Mid_assignment_[16-32332-2]/SQL/o_computer_shop.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 27, 2020 at 04:49 PM
+-- Generation Time: Dec 04, 2020 at 11:05 AM
 -- Server version: 10.4.13-MariaDB
 -- PHP Version: 7.4.8
 
@@ -42,7 +42,35 @@ CREATE TABLE `components` (
 --
 
 INSERT INTO `components` (`userID`, `c_name`, `c_catagories`, `c_types`, `c_brands`, `c_m_reviews`, `c_COST`) VALUES
-(1, 'RAM', 'storage', 'permanent', 'ASUS', 'two months to import and the service is so far so great....hope for more better performances', 2100);
+(1, 'RAM', 'storage', 'permanent', 'ASUS', 'DD4 ram with latest features available for laptops', 2102),
+(2, 'Casing', 'CPU protector/Cover', 'Hardware', 'BitFenix', 'The best and greatest casing is there in our arena ', 4600);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `ram_table`
+--
+
+CREATE TABLE `ram_table` (
+  `id` int(11) NOT NULL,
+  `ram_name` varchar(100) NOT NULL,
+  `type` varchar(100) NOT NULL,
+  `capacity` varchar(100) NOT NULL,
+  `speed` varchar(100) NOT NULL,
+  `manufacturer_review` varchar(100) NOT NULL,
+  `cost` int(11) NOT NULL,
+  `picture` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `ram_table`
+--
+
+INSERT INTO `ram_table` (`id`, `ram_name`, `type`, `capacity`, `speed`, `manufacturer_review`, `cost`, `picture`) VALUES
+(2, 'Corsair Dominator Platinum', 'DDR4', '16gb', '3000mHz', 'The greatest and better upgraded versions of DDR4 has been arrived with speeds up to 3,600 MHz', 18000, 'ram-1.jpg'),
+(3, 'Corsair Vengeance LED', 'DDR4', '16GB Kit (2 x 8GB)', '3200mHz', 'Need BIOS update to run at 3200MHz...Corsair’s Vengeance LED DDR4 series also features CL16 latency ', 12500, 'ram-2.jpg'),
+(4, 'Kingston HyperX Fury', 'DDR4, RGB', '4GB, 8GB, 16GB, 32GB', 'up to 3733MHz', 'Affordable at lower speeds, 3733MHz speed is pricey, this smart DDR3 or DDR4 RAM auto-detects system', 9500, 'ram-3.jpg'),
+(5, 'G.Skill Trident Z', 'DDR4, RGB DC', '64GB (2x32GB)', '3200mHz', 'Twice the number of memory chips,Great build quality,And, when you want more RAM than you’ll know wh', 12000, 'ram-4.jpg');
 
 -- --------------------------------------------------------
 
@@ -65,7 +93,9 @@ CREATE TABLE `user_table` (
 
 INSERT INTO `user_table` (`userID`, `username`, `password`, `userType`, `email`, `picture`) VALUES
 (1, 'emon', 'emon_aiub', 'admin', 'rasjbi@yahoo.com', 'adminishere.png'),
-(2, 'r_iqbal', 'ras_aiub', 'customer', 'riqbal@linkedin.com', 'mypic.jpg');
+(2, 'r_iqbal', 'ras_aiub', 'customer', 'riqbal@linkedin.com', 'mypic.jpg'),
+(3, 'masonMount', 'mason_mount', 'admin', 'mychelsea@gmail.com', 'walzin_3.jpg'),
+(4, 'tamz', 'tmz323', 'admin', 'tamzid@gmail.com', 'npmjsapwt.jpg');
 
 --
 -- Indexes for dumped tables
@@ -76,6 +106,12 @@ INSERT INTO `user_table` (`userID`, `username`, `password`, `userType`, `email`,
 --
 ALTER TABLE `components`
   ADD PRIMARY KEY (`userID`);
+
+--
+-- Indexes for table `ram_table`
+--
+ALTER TABLE `ram_table`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `user_table`
@@ -91,13 +127,19 @@ ALTER TABLE `user_table`
 -- AUTO_INCREMENT for table `components`
 --
 ALTER TABLE `components`
-  MODIFY `userID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `userID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `ram_table`
+--
+ALTER TABLE `ram_table`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `user_table`
 --
 ALTER TABLE `user_table`
-  MODIFY `userID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `userID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
